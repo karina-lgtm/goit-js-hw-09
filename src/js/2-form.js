79be021e30bad6fce736ad3af54a9146be5da960
Karina Myronova <karinamyronova@MacBookAir.fritz.box>
2025-03-03 14:19:30 +0100

    hw9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,47 +1,49 @@
+
 const STORAGE_KEY = 'feedback-msg';
 
 const refs = {
   form: document.querySelector('.feedback-form'),
 };
 
-let arr = [];
+// Оголошуємо змінну для збереження стану форми
+let formData = loadFromLS(STORAGE_KEY) || { email: '', message: '' };
+
+// Заповнюємо поля форми при завантаженні сторінки
+initPage();
 
 refs.form.addEventListener('input', e => {
-  const email = e.currentTarget.elements.email.value.trim();
-  const message = e.currentTarget.elements.message.value.trim();
-  const data = { email, message };
-  saveToLS(STORAGE_KEY, data);
+  // Оновлюємо лише відповідне поле в об'єкті formData
+  formData[e.target.name] = e.target.value.trim();
+  saveToLS(STORAGE_KEY, formData);
 });
-
-function initPage() {
-  const formData = loadFromLS(STORAGE_KEY);
-  refs.form.elements.email.value = formData?.email || '';
-  refs.form.elements.message.value = formData?.message || '';
-}
-
-initPage();
 
 refs.form.addEventListener('submit', e => {
   e.preventDefault();
-  const email = e.currentTarget.elements.email.value.trim();
-  const message = e.currentTarget.elements.message.value.trim();
-  const data = { email, message };
-  console.log(data);
+
+  // Якщо хоча б одне поле порожнє, виводимо повідомлення і не відправляємо форму
+  if (!formData.email || !formData.message) {
+    return alert('Fill please all fields');
+  }
+
+  console.log(formData);
+
+  // Очищаємо сховище, форму та об'єкт formData
   localStorage.removeItem(STORAGE_KEY);
   refs.form.reset();
+  formData = { email: '', message: '' };
 });
 
+function initPage() {
+  refs.form.elements.email.value = formData.email || '';
+  refs.form.elements.message.value = formData.message || '';
+}
+
 function saveToLS(key, value) {
-  const jsonData = JSON.stringify(value);
-  localStorage.setItem(key, jsonData);
+  localStorage.setItem(key, JSON.stringify(value));
 }
 
 function loadFromLS(key) {
-  const body = localStorage.getItem(key);
-  try {
-    const data = JSON.parse(body);
-    return data;
-  } catch {
-    return body;
-  }
+  const data = localStorage.getItem(key);
+  return data ? JSON.parse(data) : null;
 }
+
